a1efddade34d0aa7c3620cf0836b0e3bfe470b76
Cindy Cheng <71624875+Cindy6166@users.noreply.github.com>
2022-04-25 13:15:57 +0800

    config: add Mocking Service Worker

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,10 @@ import App from './App.vue'
 import './index.css'
 import { createPinia } from 'pinia'
 import router from './router'
+import { worker } from './mocks/browser'
+
+if (process.env.NODE_ENV === 'development') {
+    worker.start()
+  }
 
 createApp(App).use(router).use(createPinia()).mount('#app')
